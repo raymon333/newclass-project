fb67151ff72b52e7c19d3f14b331b2096ac01a86
raymon333 <135172810+raymon333@users.noreply.github.com>
2023-11-21 10:34:55 +0000

    added this two

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log ("Hello CodeSpace");
+const app = require('./app'); // Importing the Express app from a file named 'app'
+const PORT = process.env.PORT || 3000; // Setting the port number, defaulting to 3000 if not provided
+
+app.listen(PORT, () => {
+    console.log(`SERVER is running at port: ${PORT}`);
+});
